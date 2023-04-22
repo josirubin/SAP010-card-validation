0f940894d2fae37d929e5f1ccf12fba0dcecb895
Josielen Rubin <josirubinc@hotmail.com>
2023-04-22 12:17:29 -0300

    inseri o input e printei o valor na tela

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 import validator from './validator.js';
 
 console.log(validator);
+
+const btn = document.querySelector("#send"); //guarda o botão
+
+btn.addEventListener("click", function() {
+
+    const nmrCartao = document.querySelector("#number"); //guarda o input
+    const value = nmrCartao.value; //guarda o valor digitado no input
+    
+    document.getElementById("result").innerHTML = value //imprime no <p> o que foi digitado no input
+})
