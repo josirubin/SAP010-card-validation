6caed4a64e1534bb28a6199ad583eb51493d02fc
Josielen Rubin <josirubinc@hotmail.com>
2023-05-03 09:35:48 -0300

    alteração de nomenclatura

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ botao.addEventListener("click", function (event) {
   if (nmrCartao.value === ""){
     return document.getElementById("resultado").innerHTML = "Digite o número do cartão";
   }
-  const texto = nmrCartao.value; //pega o botao
-  const maskedNumbers = validator.maskify(texto);  //guarda os números mascarados
-  if (validator.isValid(texto)) {  //utiliza a função de validação
+  const vlr = nmrCartao.value; //pega o botao
+  const maskedNumbers = validator.maskify(vlr);  //guarda os números mascarados
+  if (validator.isValid(vlr)) {  //utiliza a função de validação
     return document.getElementById("resultado").innerHTML = maskedNumbers + " é um cartão válido.";
   }
   return document.getElementById("resultado").innerHTML = "Esse cartão não é válido, tente novamente.";
